02a07ec24eab435ee0a1e3b95f8afd69de2767c3
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-04-02 11:22:49 +0200

    Added components homepage

---
+++ src/components/Header.vue
@@ -0,0 +1,117 @@
+<template>
+  <header class="headertext">
+    <div class="container">
+      <div
+        class="
+          d-flex
+          flex-wrap
+          align-items-center
+          justify-content-center justify-content-lg-start
+        "
+      >
+        <a
+          href="/"
+          class="
+            d-flex
+            align-items-center
+            mb-2 mb-lg-0
+            text-white text-decoration-none
+          "
+        >
+          <img
+            alt="trvl card Logo"
+            src="./../assets/logo_trvl.png"
+            width="40"
+            height="40"
+          />
+        </a>
+
+        <ul
+          class="
+            nav
+            col-12 col-lg-auto
+            me-lg-auto
+            mb-2
+            justify-content-center
+            mb-md-0
+          "
+        >
+          <li><a href="#" class="nav-link px-2 headertext">HOME</a></li>
+          <li><a href="#" class="nav-link px-2 headertext">SHOP</a></li>
+          <li><a href="#" class="nav-link px-2 headertext">KONTAKT</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input
+            type="search"
+            class="form-control form-control-white"
+            placeholder="Suche..."
+            aria-label="Suche"
+          />
+        </form>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-login">
+            Login
+          </button>
+          <button type="button" class="btn btn-signup">Registrieren</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+import { defineComponent } from "vue";
+import { BootstrapVue } from "bootstrap-vue";
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style scoped>
+
+.headertext {
+    background-color: #fff;
+    color: #707070;
+    font-weight: 600;
+}
+
+.form-control-white {
+  color: #707070;
+  background-color: #fff;
+  border-color: #707070;
+}
+.form-control-dark:focus {
+  color: #fff;
+  background-color: var(--bs-dark);
+  border-color: #fff;
+  box-shadow: 0 0 0 0.25rem rgba(255, 255, 255, 0.25);
+}
+
+.btn-signup {
+    background: #ff4e00;
+    color: #fff;
+}
+
+.login {
+    border: none;
+    background-color: transparent;
+    color: #707070;
+}
+
+.bi {
+  vertical-align: -0.125em;
+  fill: currentColor;
+}
+
+.text-small {
+  font-size: 85%;
+}
+
+.dropdown-toggle {
+  outline: 0;
+}
+</style>
+
