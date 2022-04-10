c3b43cb7eadfc127387a98afb328dd43827ce9c1
on21viktoria <viktoria.seubert@gmx.de>
2022-04-10 16:26:22 +0200

    added vuetify and changed from vue 3 to vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>trvl card</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from 'vue';
 
-export default defineComponent({
+export default Vue.extend({
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+  },
+
+  data: () => ({
+    //
+  }),
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
