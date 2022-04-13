b84710622797b7944fa97144bedfaa32fdda990d
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-04-13 15:26:59 +0200

    Created component DesignChoice

---
+++ src/components/DesignChoice.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="designchoice">
+      <div id="blanko">Mit einer Blanko-Postkarte starten</div>
+      <div id="largeletter">Mit dem Large-Letter-Design starten</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'DesignChoice',
+});
+</script>
