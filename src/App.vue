839efec34f627047a3c072b5241afbad89cdd618
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-04-13 17:05:58 +0200

    Modified DesignChoice: style & basic logic

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
+<div>
   <Header></Header>
+  <DesignChoice></DesignChoice>
+</div>
 </template>
 
 <script lang="ts">
 
 import Vue from 'vue';
 import Header from './components/Header.vue';
+import DesignChoice from './components/DesignChoice.vue';
 
 export default Vue.extend({
   name: 'App',
+
   components: {
     Header,
+    DesignChoice,
   },
 
   data: () => ({
--- src/components/DesignChoice.vue
+++ src/components/DesignChoice.vue
@@ -1,14 +1,40 @@
 <template>
+<div>
   <div class="designchoice">
-      <div id="blanko">Mit einer Blanko-Postkarte starten</div>
-      <div id="largeletter">Mit dem Large-Letter-Design starten</div>
+    <div class="choice" id="blankowrapper">
+      <button @click="onClick('blankoDesign')">
+        Mit einer Blanko-Postkarte starten
+      </button>
+    </div>
+    <div class="choice" id="largeletterwrapper">
+      <button @click="onClick('largeLetterDesign')">
+        Mit dem Large-Letter-Design starten
+      </button>
+    </div>
+  </div>
+  <div class="outputfield">
+      <p>{{ text }}</p>
+      </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
+/* eslint-disable */
+
 export default Vue.extend({
   name: 'DesignChoice',
-});
+  data: () => ({ text: '' }),
+  methods: { 
+      onClick(design: string) { 
+          if (design === 'blankoDesign') { 
+              this.text = 'Du hast das Blanko-Design ausgewählt.'; 
+              }
+        else { this.text = 'Du hast das Large-Letter-Design ausgewählt.'}  
+            } 
+        } 
+    })
+             
+
 </script>
