8aba04934a68bf97944c79477fa55cfb059c5da1
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-04-21 16:36:42 +0200

    Commented Vue Router out to be able to display component in browser.

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,21 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import VueRouter from 'vue-router';
+// import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-Vue.use(VueRouter);
+// Vue.use(VueRouter);
 
-const routes = [
-  { path: '/helloworld', component: HelloWorld },
-]
-const router = new VueRouter({
-  routes
-})
+// const routes = [
+//   { path: '/helloworld', component: HelloWorld },
+// ]
+// const router = new VueRouter({
+//   routes
+// })
 
 new Vue({
-  router,
+  // router,
   vuetify,
   render: (h) => h(App),
 }).$mount('#app');
