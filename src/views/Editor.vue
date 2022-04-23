0a0e54ba2805fe0f201307a8df93159dae56c33f
on21viktoria <viktoria.seubert@gmx.de>
2022-04-23 16:08:19 +0200

    added toolbar to Editor

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,14 +1,61 @@
 <template>
   <div class="editor-page">
-    <h1>It's me! The Editor</h1>
+    <v-main>
+      <v-container fluid id="editor-container">
+        <div v-if="!sidebar" class="editor-grid sidebar-hidden">
+          <div id="toolbar">
+            <Toolbar @showSideBar="showSideBar" class="toolbar"> </Toolbar>
+          </div>
+          <div id="postcard"></div>
+        </div>
+        <div v-if="sidebar" class="editor-grid sidebar-shown">
+          <div id="toolbar">
+            <Toolbar @showSideBar="showSideBar" class="toolbar"> </Toolbar>
+          </div>
+          <div>
+            <Sidebar
+              class="sidebar"
+              :idSidebar="sidebarContent"
+              @hideSideBar="hideSideBar()"
+            ></Sidebar>
+          </div>
+          <div id="postcard">
+            <PostcardLayout></PostcardLayout>
+            </div>
+        </div>
+      </v-container>
+    </v-main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { EventBus } from '../main';
+import Toolbar from './../components/Toolbar.vue';
+import Sidebar from './../components/Sidebar.vue';
 
 export default Vue.extend({
   name: "Editor",
+  components: {
+    Toolbar,
+    Sidebar,
+  },
+  data: () => ({
+    sidebar: false,
+    sidebarContent: '',
+    resetSelectedTool: false,
+  }),
+  methods: {
+    showSideBar(idSidebar: string) {
+      this.sidebar = true;
+      this.sidebarContent = idSidebar;
+    },
+    hideSideBar() {
+      this.sidebar = !this.sidebar;
+      this.resetSelectedTool = true;
+      EventBus.$emit('closeSideBar', this.resetSelectedTool);
+    },
+  },
 });
 </script>
 
