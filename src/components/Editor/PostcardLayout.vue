27bf44e7331352e92cad215450ff545558ae9505
on21viktoria <viktoria.seubert@gmx.de>
2022-04-25 15:43:57 +0200

    formatting changes

--- src/components/Editor/PostcardLayout.vue
+++ src/components/Editor/PostcardLayout.vue
@@ -1,58 +1,63 @@
 <template>
-    <div class="postcard-layout">
-      <div class="postcard-side" id="front">
-        <img src="./../../assets/images-trvl-card/Ampel_Philippe-Oursel.jpg" class="image-front">
-      </div>
-      <div class="postcard-side" id="back">
-        <v-container fluid id="container-rückseite-links">
-          <v-textarea 
+  <div class="postcard-layout">
+    <div class="postcard-side" id="front">
+      <img
+        src="./../../assets/images-trvl-card/Ampel_Philippe-Oursel.jpg"
+        class="image-front"
+      />
+    </div>
+    <div class="postcard-side" id="back">
+      <v-container fluid id="container-rückseite-links">
+        <v-textarea
           solo
           counter
           name="Nachrichten-Textfeld"
           label="Deine persönliche Nachricht..."
-          :rules = "rules"
-          :value = "values">
-          </v-textarea>
-        </v-container>
-        <v-container fluid id="container-rückseite-rechts">
+          :rules="rules"
+          :value="values"
+        >
+        </v-textarea>
+      </v-container>
+      <v-container fluid id="container-rückseite-rechts">
         <div class="frankierzone">
-          <img class="briefmarke" src="./../../assets/Briefmarke.png" alt="trvl-card Briefmarke">
-        </div> 
-        <div class="address-field">
-          <hr class="address-separator"/>
-          <hr class="address-separator"/>
-          <hr class="address-separator"/>
-          <hr class="address-separator"/>
+          <img
+            class="briefmarke"
+            src="./../../assets/Briefmarke.png"
+            alt="trvl-card Briefmarke"
+          />
         </div>
-        </v-container> 
-        <div class="codierzone">
-          <p>Dieser Platz muss frei bleiben.</p>
-        </div>  
+        <div class="address-field">
+          <hr class="address-separator" />
+          <hr class="address-separator" />
+          <hr class="address-separator" />
+          <hr class="address-separator" />
+        </div>
+      </v-container>
+      <div class="codierzone">
+        <p>Dieser Platz muss frei bleiben.</p>
       </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'PostcardLayout',
-  components: {
-  },
+  name: "PostcardLayout",
+  components: {},
   data: () => ({
-    rules: [(v: string|any[]) => v.length <= 500 || 'Maximal 500 characters'],
-    value: 'Hello!',
+    rules: [(v: string | any[]) => v.length <= 500 || "Maximal 500 characters"],
+    value: "Hello!",
   }),
-})
-
+});
 </script>
 
 <style>
-
 .postcard-side {
   position: relative;
   background-color: rgb(255, 255, 255);
-  border: solid rgba(112,112,112,0.5) 1px; 
+  border: solid rgba(112, 112, 112, 0.5) 1px;
   width: 622px;
   height: 442px;
   margin: 40px auto;
@@ -61,7 +66,7 @@ export default Vue.extend({
 .image-front {
   width: 600px;
   height: 420px;
-  border: solid 1px rgb(112,112,112);
+  border: solid 1px rgb(112, 112, 112);
   display: block;
   margin: 10px auto;
 }
@@ -75,7 +80,7 @@ export default Vue.extend({
 .v-textarea textarea {
   max-width: 100% !important;
   height: 366px !important;
-  border-right: solid rgb(112,112,112) 3px;
+  border-right: solid rgb(112, 112, 112) 3px;
   font-size: 14px;
   line-height: 1.25 !important;
 }
@@ -94,8 +99,14 @@ export default Vue.extend({
 
 .codierzone {
   width: 100%;
-  background-color: rgba(112,112,112, 0.1);
-  background-image: repeating-linear-gradient(45deg, transparent, transparent 2px, rgba(112,112,112,0.1) 4px, rgba(112,112,112,0.1) 5px);
+  background-color: rgba(112, 112, 112, 0.1);
+  background-image: repeating-linear-gradient(
+    45deg,
+    transparent,
+    transparent 2px,
+    rgba(112, 112, 112, 0.1) 4px,
+    rgba(112, 112, 112, 0.1) 5px
+  );
   align-content: center;
 }
 
@@ -122,7 +133,7 @@ export default Vue.extend({
   max-width: 100%;
   text-align: center;
   height: 1px;
-  border: solid rgb(112,112,112) 1px;
+  border: solid rgb(112, 112, 112) 1px;
   margin: 30px 15px;
 }
 
@@ -145,5 +156,4 @@ export default Vue.extend({
   flex-wrap: wrap;
   padding: 5px;
 }
-
 </style>
