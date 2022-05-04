b182260fcca98f36759cadd2a2d70b5cb233fdda
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-05-04 10:35:17 +0200

    Implemented messages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -36,8 +36,10 @@
           <button @click="makeToast(message = 'Hier wirst du bald die Möglichkeit haben, dich zu registrieren. Bitte hab noch ein bisschen Geduld.', options = {
           title: 'Hier entsteht ein neues Feature!',
           autoHideDelay: 50000, appendToast: append})" type="button" class="button button-signup hovereffect" style='color: #fff'>Registrieren</button>
-          <v-icon class="carticon hovereffect">mdi-cart</v-icon>
-          <v-icon class="helpicon hovereffect">mdi-help-circle-outline</v-icon>  
+          <v-icon class="carticon hovereffect" @click="makeToast(message = 'Hier geht es bald zum Warenkorb. Bitte hab noch ein bisschen Geduld.', options = {
+          title: 'Hier entsteht ein neues Feature!',
+          autoHideDelay: 50000, appendToast: append})" >mdi-cart</v-icon>
+          <a class="helpanchor hovereffect" href="https://docs.google.com/document/d/1wn9SLNurX8Eh4nTQ9y6VwFonR0hPe_QoJ0DXVWa6SGs/edit"><v-icon>mdi-help-circle-outline</v-icon></a> 
         </div>
       </div>
     </div>
