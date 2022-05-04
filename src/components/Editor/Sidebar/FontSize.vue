be0e5bc64eb3bd7dc7d10b9daa18e30d98f3dc0e
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-05-04 15:00:09 +0200

    Worked further on size logic

--- src/components/Editor/Sidebar/FontSize.vue
+++ src/components/Editor/Sidebar/FontSize.vue
@@ -7,6 +7,7 @@
 
 <script lang="ts">
 import Vue from "vue";
+import {EventBus} from "@/main";
 
 export default Vue.extend({
   name: "FontSize",
@@ -21,7 +22,16 @@ export default Vue.extend({
           { value: 'd', text: '14px' },
           { value: 'e', text: '16px' },
           { value: 'f', text: '18px' },
-        ]
+        ],
+        sizeId: '',
+        }
+    },
+    methods: {
+    changeFontSize() {
+    this.sizeId = `${this.selected}`;
+    console.log("Color has been selected", this.sizeId, this.selected);
+    EventBus.$emit('changeFontColor', this.sizeId);
+    },
       }
     }
 });
