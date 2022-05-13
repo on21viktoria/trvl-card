430d4c6949f94cc8e29fd52328ab89700f52fdab
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-05-13 16:44:07 +0200

    Styled checkbox

--- src/views/Contactform.vue
+++ src/views/Contactform.vue
@@ -96,8 +96,10 @@
       </v-container>
     </v-form>
     <div class="saveaddress">
-    <input type="checkbox" name="Addresse speichern" value="saveaddress" class="checkbox">
-    <label for="saveaddress">Addresse speichern</label>
+    <input class="form-check-input" type="checkbox" value="Addresse speichern" id="flexCheckDefault">
+   <label class="form-check-label" for="flexCheckDefault">
+    Addresse speichern
+  </label>
     </div>
     <div class="checkoutbuttons">
         <button type="button" class="hovereffect" id="save">SICHERN & ZURÜCK</button>
@@ -283,6 +285,10 @@ h2 {
     margin: 20px;
     }
 
+.saveaddress {
+  padding-left: 25px;
+}
+
 .checkoutbuttons > button {
     margin: 20px 80px 0 0;
     padding: 15px 30px 15px 30px;
@@ -297,10 +303,6 @@ h2 {
 #pay {
     background-color: black;
     color: #fff;
-}
-
-.checkbox {
-    margin: 4px 10px 4px 0px;
 }
 
 </style>
