f13d1497ba313e0d9880dd570f2d0770d709cb9d
Robin Kauffeld <rob.kauffeld.21@lehre.dhbw.mosbach.de>
2022-05-17 09:16:51 +0200

    Implementes Logic

--- src/components/Editor/Sidebar/Upload.vue
+++ src/components/Editor/Sidebar/Upload.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
-    <!-- Styled -->
-    <b-form-file accept="image/jpeg, image/png" v-model="file1" class="mt-3" plain></b-form-file>
-    <div class="mt-3">Selected file: {{ file1 ? file2.name : '' }}</div>
+    <b-form-file @change="onFileSelected" accept="image/jpeg, image/png" v-model="file1" class="mt-3" plain></b-form-file>
+    <div class="mt-3">Deine ausgewählte Datei: {{ file1 ? file1.name : '' }}</div>
+    <button @click="onUpload">Upload</button>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
     data() {
       return {
         file1: null,
+        selectedFile: null,
       }
+    },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0]
+    },
+    onUpload() {
+      const fd = new FormData();
+      fd.append("image", this.selectedFile, this.selectedFile.name)
+      axios.post("https://us-central1-kundenuploads.cloudfunctions.net/uploadFile", fd)
+      .then(res => {
+        console.log(res)
+      })
+
     }
+  }
+
+
   }
 </script>
