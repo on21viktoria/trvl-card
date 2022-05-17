291a56752095c1e177e1f361e243374002bfa701
ON21IK <isa.koecher.21@lehre.mosbach.dhbw.de>
2022-05-17 10:43:04 +0200

    Added input field fpr customized large-letter design and styled it.

--- src/components/Editor/Sidebar/Text.vue
+++ src/components/Editor/Sidebar/Text.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
     <p class="tool-title">Text</p>
+    <p class="subtitle">Vorderseite der Postkarte</p>
+    <v-divider id="divider-sidebar"></v-divider>
+    <p>
+      Dein Urlaubsort ist nicht in unserer Sammlung dabei? <br />Dann gebe hier
+      deinen Ort ein:
+    </p>
+    <div class="input-container">
+      <input
+        id="front-input-field"
+        type="text"
+        placeholder="Grüße aus..."
+        aria-label="Grüße aus..."
+        aria-describedby="basic-addon2"
+      />
+      <!-- <div class="checkbox-container"> -->
+      <p id="display-paragraph">Auf Postkarte anzeigen:</p>
+      <label class="custom-checkbox-label">
+      <input type="checkbox" checked="checked" id="initial-checkbox"> <span id="front-checkbox"></span>
+      </label>
+      <!-- </div> -->
+    </div>
+    
     <p class="subtitle">Rückseite der Postkarte</p>
     <v-divider id="divider-sidebar"></v-divider>
     <FontChoice></FontChoice>
@@ -21,7 +43,7 @@ export default Vue.extend({
   components: {
     FontSize,
     FontChoice,
-    FontColor
+    FontColor,
   },
   props: {},
   methods: {},
@@ -40,6 +62,82 @@ export default Vue.extend({
   font-size: 15px;
   text-align: center;
   margin-bottom: 0;
+}
+
+.input-container {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+#front-input-field {
+  width: 100%;
+  margin: 0 8px 5px 0;
+  padding: 6px 28px 6px 12px;
+  font-family: "Montserrat", sans-serif;
+  font-size: 13px;
+  background-color: rgba(112, 112, 112, 0.1);
+  border-radius: 5px !important;
+  box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%),
+    0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+}
+
+#front-input-field:focus-visible {
+  outline: none;
+}
+
+#display-paragraph {
+  margin: 0 10px 0 0;
+}
+
+.custom-checkbox-label {
+  display: block;
+  position: relative;
+  cursor: pointer;
+  font-size: 13px;
+  /* width: 25px;
+  height: 25px;
+  border: 1px red solid; */
+}
+
+#initial-checkbox {
+  visibility: hidden;
+}
+
+#front-checkbox {
+  position: absolute;
+  top: 7px;
+  left: 0;
+  width: 15px;
+  height: 15px;
+  border: 1px solid grey;
+  cursor: pointer;
+}
+
+.custom-checkbox-label #initial-checkbox:checked ~ #front-checkbox {
+  background-color:#ff4e00 !important;
+}
+
+#front-checkbox:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+.custom-checkbox-label #initial-checkbox:checked ~ #front-checkbox:after {
+  display: block;
+}
+
+.custom-checkbox-label #front-checkbox:after {
+  left: 4px;
+  bottom: 1.5px;
+  width: 6px;
+  height: 12px;
+  border: solid white;
+  border-width: 0 4px 4px 0;
+  -webkit-transform: rotate(45deg);
+        -ms-transform: rotate(45deg);
+        transform: rotate(45deg);
 }
 
 #divider-sidebar {
