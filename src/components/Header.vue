d86daaad5aef12e502aa31c25728ee54be9dd610
ON21MB <mar.bauer.21@lehre.mosbach.dhbw.de>
2022-05-18 17:06:22 +0200

    Redirected FAQ to new page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,7 +35,7 @@
           <v-icon class="carticon hovereffect" @click="makeToast(message = 'Hier geht es bald zum Warenkorb. Bitte hab noch ein bisschen Geduld.', options = {
           title: 'Hier entsteht ein neues Feature!',
           autoHideDelay: 5000, appendToast: append})" >mdi-cart</v-icon>
-          <a class="helpanchor hovereffect" href="https://docs.google.com/document/d/1wn9SLNurX8Eh4nTQ9y6VwFonR0hPe_QoJ0DXVWa6SGs/edit"><v-icon>mdi-help-circle-outline</v-icon></a> 
+          <a class="helpanchor hovereffect" target="_blank" href="https://docs.google.com/document/d/1wn9SLNurX8Eh4nTQ9y6VwFonR0hPe_QoJ0DXVWa6SGs/edit"><v-icon>mdi-help-circle-outline</v-icon></a> 
         </div>
       </div>
     </div>
